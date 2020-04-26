78aff4762829422ed495bbd88ef342d4137abb2e
Aaron Mooney <aaron.mooney@errigal.com>
2020-04-26 22:37:50 +0100

    Added addition/removal of user favorites fo TV and Movies

--- movie-api/api/users/index.js
+++ movie-api/api/users/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import User from './userModel'
 import Movie from './../movies/movieModel'
+import TvShow from './../tv/tvShowModel'
 import jwt from 'jsonwebtoken';
 
 const router = express.Router() // eslint-disable-line
@@ -50,21 +51,90 @@ router.post('/', (req, res, next) => {
   }});
 
 // Update a user
-router.put('/:id',  (req, res, next) => {
-    if (req.body._id) delete req.body._id
-    User.update({
-      _id: req.params.id,
-    }, req.body, {
-      upsert: false,
-    })
-    .then(user => res.json(200, user)).catch(next)
+router.put('/:userName',  (req, res, next) => {
+  let userName = req.params.userName;
+  if (req.body._id) delete req.body._id
+  User.findByUserName(userName)
+  .then(user => {
+    user.password = req.body.password;
+    user.movieFavorites = req.body.movieFavorites;
+    user.tvShowFavorites = req.body.tvShowFavorites;
+    return user.save().then(user => res.status(200).send(user))
+  }).catch(next)
 })
 
-router.get('/:userName/favorites', (req, res, next) => {
+router.get('/:userName/favorites/movies', (req, res, next) => {
   const userName = req.params.userName;
-  User.findByUserName(userName).populate('favorites').then(
-      user => res.status(201).send(user.favorites)
+  console.log("getting fav movies for " + userName )
+  User.findByUserName(userName).populate('movieFavorites').then(
+      user => res.status(201).send(user.movieFavorites)
   ).catch(next)
 })
+
+router.get('/:userName/favorites/tv', (req, res, next) => {
+  const userName = req.params.userName;
+  User.findByUserName(userName).populate('tvShowFavorites').then(
+      user => res.status(201).send(user.tvShowFavorites)
+  ).catch(next)
+})
+
+router.put('/:userName/favorites/movies', (req, res, next) => {
+  const newMovie = req.body;
+  const userName = req.params.userName;
+
+  if (newMovie && newMovie.id){
+    Movie.findOneAndUpdate({id: newMovie.id}, newMovie, {new: true, upsert: true}).then(movie => {
+      if (!movie) return res.status(404).send({error: "Movie not found!"});
+      User.findByUserName(userName).then( user => {
+        (user.movieFavorites.indexOf(movie._id) > -1) ? user : user.movieFavorites.push(movie._id.toString());
+        user.save().then(user => res.status(201).send(user));
+      });
+    }).catch(next);
+  } else {
+    res.status(401).send("Unable to add favorite - data incomplete")
+  }
+})
+
+router.put('/:userName/favorites/tv', (req, res, next) => {
+  const newTvShow = req.body;
+  const userName = req.params.userName;
+
+  if (newTvShow && newTvShow.id){
+    TvShow.findOneAndUpdate({id: newTvShow.id}, newTvShow, {new: true, upsert: true}).then(tvShow => {
+      if (!tvShow) return res.status(404).send({error: "tvShow not found!"});
+      User.findByUserName(userName).then( user => {
+        (user.tvShowFavorites.indexOf(tvShow._id) > -1) ? user : user.tvShowFavorites.push(tvShow._id.toString());
+        user.save().then(user => res.status(201).send(user));
+      });
+    }).catch(next);
+  } else {
+    res.status(401).send("Unable to add favorite - data incomplete")
+  }
+})
+
+router.delete('/:userName/favorites/movies/:id', (req, res, next) => {
+  const userName = req.params.userName;
+  const id = req.params.id;
+  Movie.findByMovieDBId(id).then(movie => {
+    User.findByUserName(userName).then(user => {
+      const favorite = user.movieFavorites.indexOf(movie._id);
+      favorite > -1 ? user.movieFavorites.splice(favorite, 1) : user;
+      user.save().then(user => res.status(200).send(user));
+    }).catch(next);
+  }).catch(next);
+})
+
+router.delete('/:userName/favorites/tv/:id', (req, res, next) => {
+  const userName = req.params.userName;
+  const id = req.params.id;
+  TvShow.findByTvShowDBId(id).then(tvShow => {
+    User.findByUserName(userName).then(user => {
+      const favorite = user.tvShowFavorites.indexOf(tvShow._id);
+      favorite > -1 ? user.tvShowFavorites.splice(favorite, 1) : user;
+      user.save().then(user => res.status(200).send(user));
+    }).catch(next);
+  }).catch(next);
+})
+
 
 export default router
--- movie-api/api/users/userModel.js
+++ movie-api/api/users/userModel.js
@@ -5,43 +5,43 @@ import bcrypt from 'bcrypt-nodejs';
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true },
-  favorites: [{type: mongoose.Schema.Types.ObjectId,
-              ref: 'Movie'  }]
+    username: { type: String, unique: true, required: true},
+    password: {type: String, required: true },
+    movieFavorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'  }],
+    tvShowFavorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'TvShow'  }]
 });
 
 UserSchema.statics.findByUserName = function(username) {
-  return this.findOne({ username: username})
+    return this.findOne({ username: username})
 };
 
 UserSchema.methods.comparePassword = function(passw, cb) {
-  bcrypt.compare(passw, this.password, (err, isMatch) => {
-      if (err) {
-          return cb(err);
-      }
-      cb(null, isMatch);
-  });
+    bcrypt.compare(passw, this.password, (err, isMatch) => {
+        if (err) {
+            return cb(err);
+        }
+    cb(null, isMatch);
+    });
 };
 
 UserSchema.pre('save', function(next) {
-  const user = this;
-  if (this.isModified('password') || this.isNew) {
-      bcrypt.genSalt(10, (err, salt)=> {
-          if (err) {
-              return next(err);
-          }
-          bcrypt.hash(user.password, salt, null, (err, hash)=> {
-              if (err) {
-                  return next(err);
-              }
-              user.password = hash;
-              next();
-          });
-      });
-  } else {
-      return next();
-  }
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, (err, salt)=> {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, null, (err, hash)=> {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+} else {
+    return next();
+}
 });
 
 export default mongoose.model('User', UserSchema)
